c2c85417fa598c68288a03eef28a5a7d10109784
Aby Leyva <abyleyva75@gmail.com>
2026-02-18 16:52:41 -0600

    Creando plantilla, conociendo las generalidades de un proyecto  webapi

--- obj/Debug/net10.0/WebApi2026.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApi2026.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi2026")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+535c28b1abc9df37ab1cd52f65ddececaf46cf4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi2026")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi2026")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
